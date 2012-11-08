2f7759397a474b07265a2382f6726d3eab9775be
Amiel Martin <amiel@carnesmedia.com>
2012-11-08 14:30:19 -0800

    Better menu label

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,7 +1,6 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
-
+  menu label: 'Icons'
 
   sidebar 'The codes' do
     para "Want to see how this works?"
@@ -10,7 +9,7 @@ ActiveAdmin.register_page "Dashboard" do
     end
   end
 
-  content :title => 'ActiveAdmin Icons' do
+  content title: 'ActiveAdmin Icons' do
     div do
       ActiveAdmin::Iconic::ICONS.each do |key, _|
         div style: "float: left; width: 12em; padding: 2px 3px; font-size: 1.6em; line-height: 1.2em;" do
