488c81e0bd17ec68f274396d6e532077dabd03e4
Amiel Martin <amiel@carnesmedia.com>
2012-11-08 14:27:57 -0800

    Better documentation

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,6 +2,14 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
+
+  sidebar 'The codes' do
+    para "Want to see how this works?"
+    para do
+      link_to 'Check it out on github', 'https://github.com/amiel/activeadmin-icons'
+    end
+  end
+
   content :title => 'ActiveAdmin Icons' do
     div do
       ActiveAdmin::Iconic::ICONS.each do |key, _|
